f8d24b666a85c793f3e9b3fd58236dea905e23b8
“Nayonna <Nayonna Purnell>
2020-03-06 00:56:48 -0500

    Added the age calculations

--- age_calc.py
+++ age_calc.py
@@ -5,6 +5,10 @@ yearOfBirth = int(input("What year were you born?"))
 
 # Step 2
 # Calculate and print the year they'll turn 25, 50, 75, and 100.
+ageAt25 = int(25 + yearOfBirth)
+ageAt50 = int(50 + yearOfBirth)
+ageAt75 = int(75 + yearOfBirth)
+ageAt100 = int(100 + yearOfBirth)
 
 # Step 3
 # If they're already past any of these ages, skip them.
