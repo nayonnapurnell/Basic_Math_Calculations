8c17adee504d413e6ff58cc69dd2bb9498abdbc3
“Nayonna <Nayonna Purnell>
2020-03-06 00:51:09 -0500

    Created the user input prompt

---
+++ age_calc.py
@@ -0,0 +1,10 @@
+# Step 1
+# Ask the user for their name and the year they were born.
+name = input("What is your name?")
+yearOfBirth = int(input("What year were you born?"))
+
+# Step 2
+# Calculate and print the year they'll turn 25, 50, 75, and 100.
+
+# Step 3
+# If they're already past any of these ages, skip them.
